75e8b13e015cf0f4f5fe05c3eabf74e3eb9a9586
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-06-29 17:50:19 +0300

    Updates

---
+++ precache-manifest.5f4a924b7727668aa8ae693a8cec9365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab845a6bc700064628703b52981d3ff3",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "a01e4e089f3811966eaa",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.1e8c6062.chunk.css"
+  },
+  {
+    "revision": "ee8097ccbbb26e41224c",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.f28019e1.chunk.js"
+  },
+  {
+    "revision": "a01e4e089f3811966eaa",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.97e7f25a.chunk.js"
+  },
+  {
+    "revision": "671ff8e999e03896268d",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.0f0e45e8.js"
+  }
+]);
